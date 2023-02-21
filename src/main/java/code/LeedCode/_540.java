16839ca562d36a7e62973699cd3b3707fb6c9088
Alexey Belyaev <>
2023-02-21 14:03:36 +0300

    540

---
+++ src/main/java/code/LeedCode/_540.java
@@ -0,0 +1,40 @@
+package code.LeedCode;
+
+public class _540 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 1, 2, 3, 3, 4, 4, 5, 5};
+        int[] nums1 = new int[]{3};
+        int[] nums2 = new int[]{1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6};
+
+        System.out.println(singleNonDuplicate(nums1));
+    }
+
+    private static int singleNonDuplicate(int[] nums) {
+        int pos = 0;
+        int low = 0;
+        int high = nums.length - 1;
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        if (nums[0] != nums[1]) {
+            return nums[0];
+        }
+        if (nums[high] != nums[high - 1]) {
+            return nums[high];
+        }
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            if ((nums[mid] != nums[mid - 1]) && (nums[mid] != nums[mid + 1])) {
+                return nums[mid];
+            }
+            if (((mid & 1) != 0 && (nums[mid] == nums[mid - 1]))
+                    || ((mid & 1) == 0 && (nums[mid] == nums[mid + 1]))) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
